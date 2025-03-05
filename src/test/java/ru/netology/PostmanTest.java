b1de23838574f4eaec4586fb34adc440c66932ce
Yegor <yegor.belikin@gmail.com>
2025-03-05 15:15:34 +0500

    Postman test failure

---
+++ src/test/java/ru/netology/PostmanTest.java
@@ -0,0 +1,29 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+    @Test
+            void PostmanPost() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Egor") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Ego"))
+        ;
+
+
+    }
+
+
+}
