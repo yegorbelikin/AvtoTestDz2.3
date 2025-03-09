6aeefb56b1b55110703586636b148b185917252e
Yegor <yegor.belikin@gmail.com>
2025-03-09 09:54:18 +0500

    Corrected PostmanTest

--- src/test/java/ru/netology/PostmanTest.java
+++ src/test/java/ru/netology/PostmanTest.java
@@ -19,7 +19,7 @@ public class PostmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Ego"))
+                .body("data", equalTo("Egor"))
         ;
 
 
